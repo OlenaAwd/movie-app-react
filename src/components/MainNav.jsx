4ed95db128191edb6a1a4df14e934619c000fd32
olena <elena.avdiunina@gmail.com>
2022-12-01 17:59:05 +0200

    edititng

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,70 +1,80 @@
-import React, {useState, useEffect} from 'react';
-import { makeStyles } from '@mui/styles';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import MovieIcon from '@mui/icons-material/Movie';
-import TVIcon from '@mui/icons-material/Tv';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import SearchIcon from '@mui/icons-material/Search';
-import {useNavigate} from 'react-router-dom'
-
-
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@mui/styles";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import MovieIcon from "@mui/icons-material/Movie";
+import TVIcon from "@mui/icons-material/Tv";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
+import { trendingStyle } from "../constants/styles/trendingStyle";
 
 const useStyles = makeStyles({
-    root: {
-        width: '100%',
-        position: 'fixed',
-        bottom: 0,
-        backgroundColor: '#2d313a',
-        zIndex: 100,
-        color: '#fff'
-    }
-})
-
-
-
+  root: {
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
+    backgroundColor: "#2d313a",
+    zIndex: 100,
+    color: "#fff",
+  },
+});
 
 export default function SimpleBottomNavigation() {
-    const classes = useStyles();
+  const classes = useStyles();
   const [value, setValue] = useState(0);
-  const navigate= useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if(value === 0){
-        navigate("/")
-    }else if (value === 1){
-        navigate("/movies")
-    }else if (value === 2){
-        navigate("/series")
-    }else if (value === 3){
-        navigate("/search")
+    if (value === 0) {
+      navigate("/");
+    } else if (value === 1) {
+      navigate("/movies");
+    } else if (value === 2) {
+      navigate("/series");
+    } else if (value === 3) {
+      navigate("/search");
     }
-    
-      
-  }, [value, navigate])
+  }, [value, navigate]);
 
   return (
-      <BottomNavigation
-        showLabels
-        className={classes.root}
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        style={{
-            color: 'white',  width: '100%',
-            position: 'fixed',
-            bottom: 0,
-            backgroundColor: '#2d313a',
-            zIndex: 100
-        }}
-        
-      >
-        <BottomNavigationAction style={{color: 'white'}} label="Trending" icon={<WhatshotIcon />} />
-        <BottomNavigationAction style={{color: 'white'}} label="Movies" icon={<MovieIcon />} />
-        <BottomNavigationAction style={{color: 'white'}} label="TV Series" icon={<TVIcon />} />
-        <BottomNavigationAction style={{color: 'white'}} label="Search" icon={<SearchIcon />} />
-    
-      </BottomNavigation>
+    <BottomNavigation
+      showLabels
+      className={classes.root}
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      style={{
+        color: "white",
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        backgroundColor: "#2d313a",
+        zIndex: 100,
+      }}
+    >
+      <BottomNavigationAction
+        sx={trendingStyle}
+        // style={{ color: "white" }}
+        label="Trending"
+        icon={<WhatshotIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TVIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
+    </BottomNavigation>
   );
 }
---
+++ src/constants/styles/trendingStyle.js
@@ -0,0 +1,3 @@
+export const trendingStyle = {
+  color: "fff",
+};
